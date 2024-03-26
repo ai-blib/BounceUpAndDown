624e4ac1108512db28c00b61aaf2c85853ca0789
hanyuhui <1608407759@qq.com>
2024-03-26 20:50:34 +0800

    update script

--- index.639f7.js
+++ index.639f7.js
@@ -1,4 +1,4 @@
-System.register(["./application.js"], function (_export, _context) {
+System.register(["./application.f91f0.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
@@ -8,8 +8,8 @@ System.register(["./application.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_applicationJs) {
-      Application = _applicationJs.Application;
+    setters: [function (_applicationF91f0Js) {
+      Application = _applicationF91f0Js.Application;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
